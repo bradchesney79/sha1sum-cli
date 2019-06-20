75a1a55a3bc5cdf87b106c3142b24c439bcfc7f8
alex <alex@rocketguys.com>
2019-06-20 17:20:13 +0300

    fix

--- bin/sha1sum.js
+++ bin/sha1sum.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
+
 var program = require('commander');
 var shasum = require('shasum');
 var fs = require('fs');
-const read = (name) => fs.readFileSync(name, 'utf8');
+var path = require('path');
+const read = (name) => fs.readFileSync(path.resolve(process.cwd(), name), 'utf8');
 
 program
   .option('-c, --check', 'read SHA1 sums from the FILEs and check them')
   .option('-q, --quiet', 'don\'t print OK for each successfully verified file')
   .arguments('<file>')
   .action(function(file, opts) {
-    if (opts.check) return process.exit(check(file) ? 0 : 1);
-  
+    if (opts.check) return process.exit(check(file, opts) ? 0 : 1);
+
     var hash = shasum(read(file));
     if (!hash) throw new Error('hash is empty');
     console.log(hash, file);
@@ -20,9 +22,9 @@ program
   .parse(process.argv);
 
 // parse sha1sum file format: <hash><space><original file name>
-function check (file) {
-  const [origHash, origFile] = read(file).split(' ');
+function check (file, { quiet = false } = {}) {
+  const [origHash, origFile] = read(file).trim().split(' ');
   const actualHash = shasum(read(origFile));
-  if (!opts.quiet) console.log(`${ origFile }: OK`);
+  if (!quiet) console.log(`${ origFile }: OK`);
   return origHash === actualHash;
 }
